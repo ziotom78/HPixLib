b2098706a6d2a53aa255cf1610e5e0e6506190e0
Maurizio Tomasi <ziotom78@gmail.com>
2013-05-24 13:03:23 +0200

    'static inline' -> 'inline' for mollweide_is_uv_inside.

--- src/mollweide_projection.c
+++ src/mollweide_projection.c
@@ -49,7 +49,7 @@ mollweide_xy_to_uv(const hpix_bmp_projection_t * proj,
 /**********************************************************************/
 
 
-static inline _Bool
+static _Bool
 mollweide_is_uv_inside(double u, double v)
 {
     return u*u/4 + v*v < 1;
